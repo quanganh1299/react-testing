b3ce41367b3d7cd044959b0a5a6f6ca07f12dbae
quanganh1299 <quanganhdoan5599@gmail.com>
2026-01-17 17:52:07 +0700

    feat: add thresold coverage in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
         coverage: {
             include: ['src/components/**/*.{ts,tsx}'],
             exclude: ['src/components/**/*.{types,stories,constant,test,spec}.{ts,tsx}'],
+            thresholds: {
+                lines: 80,
+                functions: 80,
+                branches: 80,
+                statements: 80
+            }
         },
     },
 });
