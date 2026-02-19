be673320aa761b554cc9fe09af445d6cb43eca31
quanganh1299 <quanganhdoan5599@gmail.com>
2026-02-19 10:54:01 +0700

    update: update msw error testing

---
+++ src/components/counter-two/CounterTwo.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import { CounterTwo } from './CounterTwo'
+import { vi } from 'vitest'
+
+test('renders correctly', () => {
+    render(<CounterTwo count={0} />)
+    const textElement = screen.getByText('Counter Two')
+    expect(textElement).toBeInTheDocument()
+})
+
+test('handlers are called', async () => {
+    user.setup()
+    const incrementHandler = vi.fn();
+    const decrementHandler = vi.fn();
+    render(<CounterTwo count={0} handleIncrement={incrementHandler} handleDecrement={decrementHandler} />)
+    const incrementButton = screen.getByRole('button', { name: 'Increment' })
+    const decrementButton = screen.getByRole('button', { name: 'Decrement' })
+    await user.click(incrementButton);
+    await user.click(decrementButton);
+    expect(incrementHandler).toHaveBeenCalledTimes(1);
+    expect(decrementHandler).toHaveBeenCalledTimes(1);
+})
